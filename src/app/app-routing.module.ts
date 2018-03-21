9013aed5763eb3f870b5f3be5569f26b9ccd5af7
pugmarx <naqi.rizvi@gmail.com>
2018-03-22 00:29:01 +0530

    basic modifications for students

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 const routes : Routes = [
-{ path: 'heroes', component: HeroesComponent },
+{ path: 'students', component: HeroesComponent },
 { path: 'dashboard', component: DashboardComponent},
 { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
 { path: 'detail/:id', component: HeroDetailComponent }
